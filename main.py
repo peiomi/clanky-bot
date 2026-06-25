2b5f3e9a0b8ed841eb7497c9eaab122f36d4d078
peiomi <paydonriley@gmail.com>
2026-06-25 16:18:31 -0500

    remote

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import argparse
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("api key not found")
+
+    client = genai.Client(api_key=api_key)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        contents=messages
+    )
+    if response.usage_metadata is not None and args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    elif response.usage_metadata is None:
+        raise RuntimeError("failed api request")
+
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
